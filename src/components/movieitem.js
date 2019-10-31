e75caea29e5a964f45a33d2f281cbb20170a1e3f
ALEKSANDRA RZEPKA - STUDENT <G00357609@gmit.ie>
2019-10-31 14:34:48 +0000

    first commit

---
+++ src/components/movieitem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+class MovieItem extends React.Component {
+    render() {
+        return (
+            <div>
+                {/* <h4>{this.props.movie.Title}</h4>
+                <p>{this.props.movie.Year}</p>
+               </div> <img src={this.props.movie.Poster}></img> */}
+                <Card>
+                    <Card.Header>{this.props.movie.title}</Card.Header>
+                    <Card.Body>
+                        <blockquote className="blockquote mb-0">
+                            
+                        <img src={this.props.movie.poster}></img>
+                            <footer className="blockquote-footer">
+                            {this.props.movie.year}
+                            </footer>
+                        </blockquote>
+                    </Card.Body>
+                </Card>
+
+            </div>
+        )
+    }
+}
+export default MovieItem;
